01feb0e783f3144c037fb5738d9f5fa8ce5a22b2
Mariam <mbakhet@academic.rrc.ca>
2021-02-04 14:27:21 -0600

    Created Controllers and views for Movie and Producation_Company

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'production_companies/index'
+  get 'production_companies/show'
+  get 'movies/index'
+  get 'movies/show'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+Movie.delete_all
+ProductionCompany.delete_all
+
+# Rails.root... 0/db/top_movies.csv
+filename = Rails.root.join("db/top_movies.csv")
+
+puts "Importing CSV data form: #{filename}"
+
+csv_data = File.read(filename)
+movies = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+movies.each do |m|
+  production_company = ProductionCompany.find_or_create_by(name: m["production_company"])
+
+  if production_company&.valid?
+    movie = production_company.movies.create(
+      title:        m["original_title"],
+      year:         m["year"],
+      duration:     m["duration"],
+      description:  m["description"],
+      average_vote: m["avg_vote"]
+    )
+  else
+    puts "Invalid Production Company: #{m['production_company']} for movie #{m['original_title']} "
+  end
+end
+puts "Created #{ProductionCompany.count} Production Companies"
+puts "Created #{Movie.count} Movies"
