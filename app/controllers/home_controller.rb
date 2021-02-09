0eed76773c0ff9904fbd1c025212581b6560fc43
Mariam <mbakhet@academic.rrc.ca>
2021-02-09 16:22:01 -0600

    Tuesday class

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,13 @@ class HomeController < ApplicationController
                    .order("average_vote DESC")
                    .limit(10)
 
-    @production_companies = ProductionCompany.select("production_companies.*")
-                                             .select("COUNT(production_companies.id) as movie_count")
-                                             .joins(:movies)
-                                             .group("production_companies.id")
-                                             .order("movie_count DESC")
+    @production_companies = ProductionCompany.ordered_by_movies
                                              .limit(10)
+
+    # SELECT COUNT(*) as movie_count, production_companies.name, production_companies.id
+    # FROM production_companies
+    # JOIN movies ON production_companies.id = movies.production_company_id
+    # GROUP BY production_companies.name
+    # ORDER BY movie_count DESC;
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,13 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.includes(:production_company).all.order("average_vote DESC")
+    @movies = Movie.all
+    # @movies is passed to the view for Movies#index
   end
 
   def show
+    # params[:id] is the value after the forward slash in the url:
+    # example: http://localhost:3000/movies/1   <== the 1 is the :id value
     @movie = Movie.find(params[:id])
+    # @movie is passed to the view for Movies#show
   end
 end
