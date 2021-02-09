e85a50ad3d5697856d1772e20141c7c7d8565f9f
Mariam <mbakhet@academic.rrc.ca>
2021-02-09 15:23:10 -0600

    Refactored Production_company method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,12 @@ class HomeController < ApplicationController
     @movies = Movie.includes(:production_company)
                    .order("average_vote DESC")
                    .limit(10)
-    @production_companies = ProductionCompany.limit(10)
+
+    @production_companies = ProductionCompany.select("production_companies.*")
+                                             .select("COUNT(production_companies.id) as movie_count")
+                                             .joins(:movies)
+                                             .group("production_companies.id")
+                                             .order("movie_count DESC")
+                                             .limit(10)
   end
 end
